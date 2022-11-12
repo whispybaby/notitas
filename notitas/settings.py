55e89b0a9e31ed0848f342cd49b62462aa52c3a2
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-11-12 18:23:44 -0300

    Configuración de la base de datos

--- notitas/settings.py
+++ notitas/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'notitas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'notitas.cnf'
+        }
     }
 }
 

513573aee541c375fe39f361c53c8c7a61d03fbe
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-11-12 22:25:31 -0300

    Tipos de mantenciones

---
+++ tipos_mantenciones/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from tipos_mantenciones.models import TipoMantencion
+
+admin.site.register(TipoMantencion)
---
+++ tipos_mantenciones/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TiposMantencionesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tipos_mantenciones"

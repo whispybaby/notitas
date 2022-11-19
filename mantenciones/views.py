a3c6bcdef1866a4a3149380995915c399de20fb3
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-11-19 16:58:35 -0300

    Crear mantenciones desde cada vehículo

--- mantenciones/views.py
+++ mantenciones/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from mantenciones.forms import DetalleMantencionFormulario
 from vehiculos.models import Vehiculo
 
@@ -11,10 +12,14 @@ def registrar(request, id_vehiculo):
         formulario = DetalleMantencionFormulario(request.POST)
 
         if formulario.is_valid():
-            print(formulario.cleaned_data)
-        
+            mantencion = formulario.save(commit=False)
+            mantencion.vehiculo = vehiculo
+            mantencion.save()
+            return redirect(reverse('vehiculos:vehiculo', args=[id_vehiculo]))
         else:
-            print(request.POST)
+            return render(request, 'mantenciones/crear.html', {
+                'formulario': formulario
+            })
 
     return render(request, 'mantenciones/crear.html', {
         'formulario': formulario

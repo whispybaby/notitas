cdf4e310a4491fb8554a222df72ea0f99ef29a74
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-11-19 21:15:57 -0300

    Restringir contraseñas para aumentar seguridad

--- notitas/helpers.py
+++ notitas/helpers.py
@@ -10,3 +10,30 @@ def inicio_obligatorio(funcion, ruta_redireccion='usuarios:iniciar'):
         else:
             return redirect(ruta_redireccion)
     return funcion_decorada
+
+
+def validar_contraseña(contraseña):
+    puntuacion = 0
+    mayusculas = 0
+    minusculas = 0
+    digitos = 0
+
+    import string
+
+    for caracter in contraseña:
+        if caracter in string.ascii_lowercase:
+            minusculas += 1
+
+        if caracter in string.ascii_uppercase:
+            mayusculas += 1
+
+        if caracter in string.punctuation:
+            puntuacion += 1
+
+        if caracter in string.digits:
+            digitos += 1
+
+    if not puntuacion or not mayusculas or not minusculas or not digitos:
+        return False
+
+    return True
